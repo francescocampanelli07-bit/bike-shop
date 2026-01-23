6a9cf630239b14b2c0dd263e4d11cff9c31ea624
francescocampanelli07-bit <francescocampanelli07@gmail.com>
2026-01-23 20:07:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,3 @@ if (navToggle && navLinks) {
     navLinks.classList.toggle("show");
   });
 }
-
-// Finto invio form contatti
-
-/*const contactForm = document.querySelector(".contact-form form");
-if (contactForm) {
-  contactForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert("Grazie per averci contattato! Ti risponderemo il prima possibile.");
-    contactForm.reset();
-  }); 
-}  */
